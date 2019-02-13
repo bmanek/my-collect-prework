30a3c32c1e2ec31a63d626051fa1de8cea05d489
Ben Manekin <ben.manekin@gmail.com>
2019-02-13 00:46:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,8 +8,8 @@ def my_collect(list)
   new_list
 end
 
-languages = ['ruby', 'javascript', 'python', 'objective-c']
-students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+#languages = ['ruby', 'javascript', 'python', 'objective-c']
+#students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
-my_collect(languages) { |a| a.upcase }
-my_collect(students) { |a| a.split" " }
+#my_collect(languages) { |a| a.upcase }
+#my_collect(students) { |a| a.split" " }
